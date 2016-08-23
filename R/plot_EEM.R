1d786044319b9574118359db4b1b8557abbe320f
adsteen <andrew.decker.steen@gmail.com>
2016-08-23 16:44:49 -0400

    start of code to manually set fill scale

--- R/plot_EEM.R
+++ R/plot_EEM.R
@@ -1,11 +1,17 @@
 ##' Plot processed or raw EEMs
 ##' @export
 
-plot_EEM <- function(EEM, plot.title="EEM plot",  contour=FALSE, save.plot=FALSE,  fn=NA, oldpar=par(), ht=4, wd=4, dpi=300, ...) {
+plot_EEM <- function(EEM, scale.max=NULL, plot.title="EEM plot",  contour=FALSE, save.plot=FALSE,  fn=NA, oldpar=par(), ht=4, wd=4, dpi=300, ...) {
   #browser()
   # check_EEM(EEM)
+  
   p <- ggplot(EEM, aes(x=ex.nm, y=em.nm, fill=RFU)) + 
     geom_tile() + 
     coord_fixed()
+  
+  if(!is.nul(scale.max)) {
+    #p <- p+
+  }
+  
   p
 }
