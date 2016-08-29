2923266bf0e119d338613f4663da50dbb1d1635e
adsteen <andrew.decker.steen@gmail.com>
2016-08-28 21:30:41 -0400

    I have no idea what the change here was about

--- R/plot_EEM.R
+++ R/plot_EEM.R
@@ -2,7 +2,7 @@
 ##' @export
 
 plot_EEM <- function(EEM, scale.max=NULL, plot.title="EEM plot",  contour=FALSE, save.plot=FALSE,  fn=NA, oldpar=par(), ht=4, wd=4, dpi=300, ...) {
-  browser()
+  # browser()
   # check_EEM(EEM)
   
   p <- ggplot(EEM, aes(x=ex.nm, y=em.nm, fill=RFU)) + 
